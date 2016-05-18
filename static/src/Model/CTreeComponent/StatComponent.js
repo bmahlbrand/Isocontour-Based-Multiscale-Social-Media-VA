d31d896766ef3bdad5cf18f17ec02dc4c06260e1
Jiawei Zhang <zhan1486@purdue.edu>
2016-05-18 12:31:23 -0400

    calculate categorical distribution of spatial clusters;

--- static/src/Model/CTreeComponent/StatComponent.js
+++ static/src/Model/CTreeComponent/StatComponent.js
@@ -1,10 +1,29 @@
-StatComponent = function(cateClusterId){
+StatComponent = function(tweets){
+	this.tweets = tweets;
+	this.cateDist = this.calCateDist(this.tweets);
+};
+
+StatComponent.prototype.calCateDist = function(){
+
+	var cateDup = [];
+	this.tweets.forEach(function(id){
+		cateDup = cateDup.concat(Object.keys(DataCenter.instance().tweets[id].cate));
+	});
 	
-	this.cateClusterId = cateClusterId;
+	//get distribution
+	var dist = _.countBy(cateDup);
 
-};
+	//add entry for the cates that are 0
+	DataCenter.instance().categories.forEach(function(cate){
 
+		if(cate in dist)
+			return;
+		dist[cate] = 0;
+	});
 
-StatComponent.prototype.getScore = function(){
-	return this.cateClusterId;
-};
+	return dist;
+}
+
+StatComponent.prototype.getCateDist = function(){
+	return this.cateDist;
+}
