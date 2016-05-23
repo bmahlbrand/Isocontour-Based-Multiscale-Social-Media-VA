0d1763f822f8d9b643a55002da794cb0488ad845
Ben Ahlbrand <bmahlbrand@gmail.com>
2016-05-23 15:02:20 -0400

    delete unnecessary directive

--- static/src/Controller/app_controller.js
+++ static/src/Controller/app_controller.js
@@ -612,14 +612,3 @@ twittNavApp.controller('ClusterSignatureCtrl', function($rootScope, $scope) {
 
 	};
 });
-
-angular.module('scroll', []).directive('whenScrolled', function() {
-    return function(scope, elm, attr) {
-        var raw = elm[0];
-        elm.bind('scroll', function() {
-            if (raw.scrollTop + raw.offsetHeight >= raw.scrollHeight) {
-                scope.$apply(attr.whenScrolled);
-            }
-        });
-    };
-});
