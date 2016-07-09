ff741f1b2547296b6e35787ac447c90ec2f5ede8
Jiawei Zhang <zhan1486@purdue.edu>
2016-07-09 15:38:04 -0400

    calculate the measurement of two categorical distribution based on the vector space model;

--- static/src/Model/CTreeComponent/StatComponent.js
+++ static/src/Model/CTreeComponent/StatComponent.js
@@ -1,29 +1,48 @@
 StatComponent = function(tweets){
 	this.tweets = tweets;
-	this.cateDist = this.calCateDist(this.tweets);
 };
 
-StatComponent.prototype.calCateDist = function(){
+//normalized distribution;
+StatComponent.prototype.calCateDist = function(cate){
 
 	var cateDup = [];
 	this.tweets.forEach(function(id){
-		cateDup = cateDup.concat(Object.keys(DataCenter.instance().tweets[id].cate));
+		cate = intersect_arrays( DataCenter.instance().focusCates, Object.keys(DataCenter.instance().tweets[id].cate) );
+		cateDup = cateDup.concat(cate);
 	});
 	
 	//get distribution
 	var dist = _.countBy(cateDup);
 
 	//add entry for the cates that are 0
-	DataCenter.instance().categories.forEach(function(cate){
+	DataCenter.instance().focusCates.forEach(function(cate){
 
 		if(cate in dist)
 			return;
 		dist[cate] = 0;
 	});
 
-	return dist;
-}
+	var values =  _.values(dist);
+	var sum = values.reduce(function(a, b) { return a + b; }, 0);
+	if(sum <= 0)
+		return dist;
 
-StatComponent.prototype.getCateDist = function(){
-	return this.cateDist;
-}
+	for(var c in dist)
+		dist[c] = dist[c] / sum;
+
+	return dist;
+};
+
+//calculate the distance between two vectors (atually two dist that has the same set of keys -- focusedCates);
+//the distance should be in the range of [0,1]
+StatComponent.vecDist = function(id1, id2){
+
+	var dist1 = DataCenter.instance().getTree().getNodeById(id1).stat.calCateDist(DataCenter.instance().focusCates);
+	var dist2 = DataCenter.instance().getTree().getNodeById(id2).stat.calCateDist(DataCenter.instance().focusCates);
+
+	var sum = 0;
+	for(var c in dist1)
+		sum += Math.pow(dist1[c]-dist2[c], 2);
+	return Math.sqrt(sum);
+
+};
